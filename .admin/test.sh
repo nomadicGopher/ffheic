1d4b3e4f989ae60a00bc8c0be98eac47cf6d8995
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2026-01-01 23:55:24 -0800

    add admin

---
+++ .admin/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../
+
+#go run main.go -input="" -output="png" # Single file to png
+#go run main.go -input="" -output="jpg" # Single file to jpg
+#go run main.go -input="" -output="jpeg" # Single file to jpeg
+
+#go run main.go -input="" -output="jpg" # Directory
