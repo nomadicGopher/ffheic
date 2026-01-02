97fa553441e2c114f3862f342e13ec5960d56edd
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2026-01-01 19:56:33 -0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#go run main.go -input="/home/orin/Pictures/20241221_074619.heic" -output="png" # Single file to png
+#go run main.go -input="/home/orin/Pictures/20241221_074619.heic" -output="jpg" # Single file to jpg
+#go run main.go -input="/home/orin/Pictures/20241221_074619.heic" -output="jpeg" # Single file to jpeg
+#go run main.go -input="/home/orin/Pictures" -output="png" # Directory
